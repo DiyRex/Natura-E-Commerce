67615ec7011aea442dc77071fa029bc4ac37893d
DiyRex <75021968+DiyRex@users.noreply.github.com>
2024-05-06 19:01:11 +0530

    Create searchFilter.java

---
+++ Natura/src/java/UrlFilters/searchFilter.java
@@ -0,0 +1,43 @@
+package UrlFilters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Devin
+ */
+@WebFilter("/pages/search_product.jsp")
+public class searchFilter implements Filter {
+     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String uri = httpRequest.getRequestURI();
+        if (uri.endsWith("/pages/search_product.jsp")) {
+            httpResponse.sendRedirect("/");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
